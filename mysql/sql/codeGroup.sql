99644c6cc0437531b641dc36e538bbad732a46d9
Laelpark <gottkfkd@naver.com>
2022-09-01 17:37:52 +0900

    2022-09-01

--- mysql/sql/codeGroup.sql
+++ mysql/sql/codeGroup.sql
@@ -2,3 +2,23 @@ SELECT * FROM per_project.codeGroup;
 
 ALTER TABLE codeGroup
 	ADD COLUMN name_eng varchar(45);
+    
+SELECT count(*)
+FROM code;
+
+SELECT
+	a.*
+	,count(b.codeGroup_ccgSeq)
+FROM 
+	codeGroup a
+JOIN code b on b.codeGroup_ccgSeq = a.ccgSeq
+group by
+b.codeGroup_ccgSeq
+;
+
+
+SELECT
+	 a.*
+	,count(b.codeGroup_ccgSeq) AS 코드갯수;
+FROM 
+	codeGroup a
