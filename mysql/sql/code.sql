ceee2f93ef8786188fac03833aabe0ddefdcbea1
Laelpark <gottkfkd@naver.com>
2022-09-01 16:37:53 +0900

    2022-09-01

--- mysql/sql/code.sql
+++ mysql/sql/code.sql
@@ -1,8 +1,12 @@
 use per_project;
 SELECT * FROM code;
+ALTER TABLE code CHANGE COLUMN codeGroupName_kr codeName varchar(45);
 
-
-    ALTER TABLE code CHANGE COLUMN codeGroupName_kr codeName varchar(45);
-    
-    
-    SELECT * from code;  
+SELECT
+	b.*
+	,a.ccgSeq
+	,a.name
+FROM 
+	code b
+JOIN codeGroup a on a.ccgSeq = b.codeGroup_ccgSeq
+;
