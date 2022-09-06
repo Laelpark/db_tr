2650fb3185ea22f1ebca2677f17da7177e42f0be
Laelpark <gottkfkd@naver.com>
2022-09-06 18:33:43 +0900

    2022-09-06

---
+++ mysql/test.sql
@@ -0,0 +1,8 @@
+select * from 테이블 이름;
+
+alter table 테이블이름 add column 칼럼이름 데이터타입;
+alter table 테이블이름 modify column 칼럼이름 데이터입;
+alter table 테이블이름 change column 칼럼이름 변경칼럼이름 데이터타입;
+alter table 테이블이름 drop column 칼럼이름;
+
+delete from
