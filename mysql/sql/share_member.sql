0f5e4cbd4bb98b6f0e4fa1693df1983eca7b5c2e
Laelpark <gottkfkd@naver.com>
2022-09-26 11:52:07 +0900

    2022-09-26

--- mysql/sql/share_member.sql
+++ mysql/sql/share_member.sql
@@ -1,1 +1,10 @@
 SELECT * FROM per_project.share_member;
+
+ALTER TABLE share_member CHANGE COLUMN userId id varchar(45);
+ALTER TABLE share_member CHANGE COLUMN memfield memField varchar(45);
+
+ALTER TABLE share_member ADD COLUMN phone_div tinyint after phone;
+
+ALTER TABLE share_member MODIFY COLUMN phone int;
+
+ALTER TABLE share_member drop COLUMN dob2;
